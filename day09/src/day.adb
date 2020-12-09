3c9248092a665a16faf5d328301c09b2870a236d
John Warwick <jwarwick@gmail.com>
2020-12-09 14:42:35 -0500

    cleanup

--- day09/src/day.adb
+++ day09/src/day.adb
@@ -6,12 +6,16 @@ package body Day is
   package TIO renames Ada.Text_IO;
 
   function load_file(filename : in String) return XMAS_Vector.Vector is
+    package Long_Integer_Text_IO is new Ada.Text_IO.Integer_IO(Long_Integer);
+
     file : TIO.File_Type;
     v : XMAS_Vector.Vector;
+    n : Long_Integer := 0;
   begin
     TIO.open(File => file, Mode => TIO.In_File, Name => filename);
     while not TIO.end_of_file(file) loop
-      v.append(Long_Integer'Value(TIO.get_line(file)));
+      Long_Integer_Text_IO.get(file, n);
+      v.append(n);
     end loop;
     TIO.close(file);
     return v;
@@ -72,7 +76,7 @@ package body Day is
   function first_invalid(v : in XMAS_Vector.Vector; preamble : in Positive) return Long_Integer is
     sums : Sum_Vectors.Vector := initialize_sums(v, preamble);
   begin
-    for idx in preamble..Natural(length(v))-1 loop
+    for idx in preamble..v.last_index loop
       if not contains(sums, v(idx)) then
         return v(idx);
       end if;
@@ -80,9 +84,6 @@ package body Day is
     end loop;
     return 0;
   end first_invalid;
-
-  -- package Running_Sum_Vectors is new Ada.Containers.Vectors (Index_Type => Natural, Element_Type => Long_Integer);
-  -- use Running_Sum_Vectors;
 
   function min_max_sum(v : in XMAS_Vector.Vector; start_idx : in Natural; end_idx : in Natural) return Long_Integer is
     min : Long_Integer := Long_Integer'Last;
@@ -100,7 +101,6 @@ package body Day is
   end min_max_sum;
 
   function find_sum(v : in XMAS_Vector.Vector; target : in Long_Integer) return Long_Integer is
-    -- sums : Running_Sum_Vectors.Vector := Running_Sum_Vectors.Empty_Vector;
     sum : Long_Integer := 0;
   begin
     for i in  v.first_index .. v.last_index loop
@@ -116,85 +116,4 @@ package body Day is
     end loop;
     return 0;
   end find_sum;
-
-  -- function find_sum(v : in XMAS_Vector.Vector; target : in Long_Integer) return Long_Integer is
-  --   sums : Running_Sum_Vectors.Vector := Running_Sum_Vectors.Empty_Vector;
-  --   last_idx : constant Natural := Natural(length(v)) - 1;
-  --   start_idx : Natural := 0;
-  --   end_idx : Natural := start_idx + 1;
-  --   sum : Long_Integer := v(start_idx);
-  --   old_start : Long_Integer := 0;
-  -- begin
-  --   loop
-  --     sum := sum + v(end_idx);
-  --     if sum = target then
-  --       return min_max_sum(v, start_idx, end_idx);
-  --     elsif sum < target then
-  --       sums.append(sum);
-  --       end_idx := end_idx + 1;
-  --     else -- > target
-  --       exit;
-  --     end if;
-  --   end loop;
-
-  --   end_idx := end_idx - 1;
-  --   for v of sums loop
-  --     TIO.put(Long_Integer'Image(v) & " ");
-  --   end loop;
-  --   TIO.new_line;
-  --   TIO.put_line(Natural'Image(start_idx) & "," & Natural'Image(end_idx));
-
-  --   loop
-  --     TIO.put_line("Getting start: " & Natural'Image(start_idx));
-  --     old_start := v(start_idx);
-  --     TIO.put_line("Got it");
-  --     start_idx := start_idx + 1;
-  --     end_idx := Natural'min(end_idx + 1, last_idx);
-  --     sums.delete(0);
-  --     for idx in sums.first_index .. sums.last_index loop
-  --       if start_idx+idx >= last_idx then
-  --         TIO.put_line("Jumping out");
-  --         exit;
-  --       end if;
-  --       TIO.put_line("Trying " & Natural'Image(start_idx) & "-" & Natural'Image(start_idx + idx));
-  --       TIO.put_line("Endidx: " & Natural'Image(end_idx));
-  --       sum := sums(idx) - old_start;
-  --       -- TIO.put_line("Targ: " & Long_Integer'Image(target) & ", sum: " & Long_Integer'Image(sum));
-  --       -- TIO.put_line("sum: " & Natural'Image(start_idx) & "-" & Natural'Image(start_idx+idx) & "=" & Long_Integer'Image(sum));
-  --       if sum = target then
-  --         return min_max_sum(v, start_idx, start_idx + idx);
-  --       end if;
-  --       if sum > target then
-  --         declare
-  --           last : constant Natural := sums.last_index;
-  --         begin
-  --           -- TIO.put_line("idx: " & Natural'Image(idx) & ",last:" & Natural'Image(last));
-  --           for di in idx..last loop
-  --             -- TIO.put_line("Targ: " & Long_Integer'Image(target) & ", sum: " & Long_Integer'Image(sum));
-  --             -- TIO.put_line("DELETING!!!!!!!!!!!!!!!!!!!!!");
-  --             sums.delete(di);
-  --           end loop;
-  --         end;
-  --       end if;
-  --     end loop;
-  --     TIO.put_line("Accessing end_idx" & Natural'Image(end_idx));
-  --     sum := sum + v(end_idx);
-  --     TIO.put_line("Got it");
-  --     if sum = target then
-  --       return min_max_sum(v, start_idx, end_idx);
-  --     end if;
-  --     if sum < target then
-  --       TIO.put_line("Adding index" & Natural'Image(end_idx));
-  --       TIO.put_line("Added " & Long_Integer'Image(sum));
-  --       sums.append(sum);
-  --       -- end_idx := end_idx + 1;
-  --       -- sum := sum + v(end_idx);
-  --     end if;
-  --     -- if target > sum + v(end_idx+1) then
-  --     --   TIO.put_line("need to add");
-  --     --   exit;
-  --     -- end if;
-  --   end loop;
-  --   -- return 8;
-  -- end find_sum;
 end Day;
